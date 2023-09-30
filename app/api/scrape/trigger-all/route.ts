0dbb366d10cd3f90d8b5ae04823236fbd1172733
Mohamed Hakem <mohamedhakem959628@gmail.com>
2023-09-30 19:21:56 +0300

    feat: add top-headlines to the categories & clean

--- app/api/scrape/trigger-all/route.ts
+++ app/api/scrape/trigger-all/route.ts
@@ -19,8 +19,11 @@ export async function HEAD(request: Request) {
   console.log('🚀 ~ file: route.ts:15 ~ HEAD ~ categories:', categories);
 
   // Generate API endpoints
-  const endpoints = categories.map((category) => `https://msnewsapi.vercel.app/api/scrape/googlenews/${category.name}`);
+  let endpoints = categories.map((category) => `https://msnewsapi.vercel.app/api/scrape/googlenews/${category.name}`);
   console.log('🚀 ~ file: route.ts:20 ~ endpoints ~ endpoints:', endpoints);
+
+  // add the (a bit different) category
+  endpoints.push('https://msnewsapi.vercel.app/api/scrape/googlenewstopheadlines');
 
   // Fetch each endpoint and check the response status.
   console.time('Promise.allSettled');
@@ -35,17 +38,6 @@ export async function HEAD(request: Request) {
   );
   console.timeEnd('Promise.allSettled');
   console.log('🚀 ~ file: route.ts:35 ~ GET ~ results:', results);
-
-  // Handle the outcome of each request.
-  // for (const result of results) {
-  //   if (result.status === 'fulfilled') {
-  //     // The request was resolved.
-  //     // TODO: Do something with the resolved value, log it to the console.
-  //   } else {
-  //     // The request was rejected.
-  //     // TODO: Do something with the resolved value, log it to the console.
-  //   }
-  // }
 
   // Return a response with the results of the checks.
   console.timeEnd('[trigger-all] HEAD');
